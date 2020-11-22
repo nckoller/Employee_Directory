21349323882d8ce7a6f7e16b92b996f8094161f2
Nina Koller <nckoller@gmail.com>
2020-11-21 22:13:56 -0600

    adding SearchBox.js

--- employee_directory/src/components/SearchBox.js
+++ employee_directory/src/components/SearchBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../App.css';
 
-function SearchBox({ handleSearchChange }) {
+export function SearchBox({ handleSearchChange }) {
   return (
     <div>
-      <form class="form-inline my-2 my-lg-0">
+      <form className="form-inline my-2 my-lg-0">
         <input
-          class="form-control mr-sm-2"
+          className="form-control mr-sm-2"
           type="search"
           placeholder="Search by name"
           onChange={(e) => handleSearchChange(e)}
@@ -16,4 +16,3 @@ function SearchBox({ handleSearchChange }) {
   );
 }
 
-export default SearchBox;
